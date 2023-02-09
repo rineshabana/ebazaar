d5d2a9d48c6b977a636f55433c65f3da27c29ce5
Faraz Noor <faraznoor75@gmail.com>
2023-02-09 23:17:53 +0530

    Ran Rails App Upgrade

--- db/schema.rb
+++ db/schema.rb
@@ -2,17 +2,24 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_26_135431) do
+ActiveRecord::Schema.define(version: 2023_02_09_174318) do
 
-  create_table "user_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "products", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price", precision: 10
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_addresses", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "user_profile_id"
     t.string "address_line1"
     t.string "address_line2"
@@ -27,7 +34,7 @@ ActiveRecord::Schema.define(version: 2023_01_26_135431) do
     t.index ["user_profile_id"], name: "index_user_addresses_on_user_profile_id"
   end
 
-  create_table "user_profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "user_profiles", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "user_id"
     t.string "first_name"
     t.string "middle_name"
@@ -41,7 +48,7 @@ ActiveRecord::Schema.define(version: 2023_01_26_135431) do
     t.index ["user_id"], name: "index_user_profiles_on_user_id", unique: true
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
