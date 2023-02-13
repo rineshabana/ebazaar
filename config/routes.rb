6692655a575ab587b8704c5ad8ef14f862bc3869
Shabana Noor <rineshabana95@gmail.com>
2023-02-14 00:23:57 +0530

    Used the resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,14 @@ Rails.application.routes.draw do
   get 'user_address/:id/edit', to: 'user_address#edit', as: 'edit_user_address'
   patch 'user_address/update/:id', to: 'user_address#update'
   delete 'user_address/:id/destroy', to: 'user_address#destroy', as: 'destroy_user_address'
-  get 'user_profile/edit'
-  get 'user_profile/show', as: 'profile_show'
-  post 'user_profile/create'
-  patch 'user_profile/update'
+  resources :user_profile, except: [:new, :edit, :index, :create, :update, :show, :destroy] do
+    collection do
+        get 'edit', as: :edit
+        get 'show', as: :show
+        post 'create', as: :create
+        patch 'update', as: :update
+    end
+  end
   devise_for :users
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
