66296f447deafc80b4adea3c6353b587bd98bde7
Faraz Noor <faraznoor75@gmail.com>
2023-02-19 00:51:52 +0530

    Made Product listing dynamic and modified root path

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :user_addresses, except: [:show]
   resource :user_profile, except: [:new, :destroy]
   devise_for :users
-  root 'home#index'
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
